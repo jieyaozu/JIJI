3d5755843605a65815a6cd602a0bb6d0c769e8a9
unknown <>
2017-04-28 08:02:08 +0800

    个人设置、系统设置选项的增加

--- app/src/main/java/com/yaozu/object/activity/setting/SettingActivity.java
+++ app/src/main/java/com/yaozu/object/activity/setting/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.yaozu.object.activity.setting;
 
 import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.yaozu.object.R;
 import com.yaozu.object.activity.BaseActivity;
@@ -9,7 +11,9 @@ import com.yaozu.object.activity.BaseActivity;
  * Created by jieyaozu on 2017/4/27.
  */
 
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
+    private TextView tvAccoutnSet, tvClearCache, tvCheckUpdate, tvAbout;
+
     @Override
     protected void setContentView() {
         setContentView(R.layout.activity_setting);
@@ -17,7 +21,10 @@ public class SettingActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        tvAccoutnSet = (TextView) findViewById(R.id.setting_account_set);
+        tvClearCache = (TextView) findViewById(R.id.setting_system_clearcache);
+        tvCheckUpdate = (TextView) findViewById(R.id.setting_checkupdate);
+        tvAbout = (TextView) findViewById(R.id.setting_about);
     }
 
     @Override
@@ -27,12 +34,29 @@ public class SettingActivity extends BaseActivity {
 
     @Override
     protected void setListener() {
-
+        tvAccoutnSet.setOnClickListener(this);
+        tvClearCache.setOnClickListener(this);
+        tvCheckUpdate.setOnClickListener(this);
+        tvAbout.setOnClickListener(this);
     }
 
     @Override
     protected void settingActionBar(ActionBar actionBar) {
         actionBar.setTitle("设置");
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.setting_account_set:
+                break;
+            case R.id.setting_system_clearcache:
+                break;
+            case R.id.setting_checkupdate:
+                break;
+            case R.id.setting_about:
+                break;
+        }
     }
 }
